1b1364f80b0ef46b1d69f74a13dd6e7947d1d3f4
Aloy <122377449+GirlxCode@users.noreply.github.com>
2023-02-21 09:11:54 -0600

    all pages drafted

---
+++ assets/js/script.js
@@ -0,0 +1,291 @@
+//all questions, answers, and option were put in an array. 
+const questions = [
+    {
+      questionText: "Javascript is what kind of side programming language?",
+      options: ["1. client", "2. sever", "3. both", "4. none"],
+      answer: "3. both",
+    },
+    {
+      questionText: "Which Javascript label catches all values except for the ones specified?",
+      options: [
+        "1. catch",
+        "2. label",
+        "3. try",
+        "4. default",
+      ],
+      answer: "4. default",
+    },
+    {
+      questionText:
+        "Which type of variable is visible only within a function that has been defined?",
+      options: ["1. local variable", "2. global variable", "3. none", "4. both"],
+      answer: "1. local variable",
+    },
+    {
+      questionText:
+        "Which of the following is a disadvantage of using Javascript?",
+      options: [
+        "1. Client-side, Javascript does not allow for reading or writing of files",
+        "2. JavaScript can not be used for Networking applications because there is no such support available.",
+        "3. JavaScript doesn't have any multithreading or multiprocess capabilities.",
+        "4. All of the above",
+      ],
+      answer: "4. All of the above",
+    },
+    {
+      questionText:
+        "Which of the following is true about cookie handling in JavaScript",
+      options: [
+        "1. JavaScript can manipulate cookies using the cookie property of the Document object.", 
+        "2. JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page. ", 
+        "3. Both",
+        "4. None",
+    ],
+      answer: "3. Both",
+    },
+  ];
+  
+  //selected each card div by id to assign to variables
+  const startCard = document.querySelector("#start-card");
+  const questionCard = document.querySelector("#question-card");
+  const scoreCard = document.querySelector("#score-card");
+  const leaderboardCard = document.querySelector("#leaderboard-card");
+  
+  //hides all cards
+  function hideCards() {
+    startCard.setAttribute("hidden", true);
+    questionCard.setAttribute("hidden", true);
+    scoreCard.setAttribute("hidden", true);
+    leaderboardCard.setAttribute("hidden", true);
+  }
+  
+  const resultDiv = document.querySelector("#result-div");
+  const resultText = document.querySelector("#result-text");
+  
+  //hide result div
+  function hideResultText() {
+    resultDiv.style.display = "none";
+  }
+  
+  //variables that are required globally
+  var intervalID;
+  var time;
+  var currentQuestion;
+  
+  document.querySelector("#start-button").addEventListener("click", startQuiz);
+  
+  function startQuiz() {
+    //hide any visible cards, shows the question card
+    hideCards();
+    questionCard.removeAttribute("hidden");
+  
+    //assign 0 to currentQuestion when start button is clicked, then display the current question on the page
+    currentQuestion = 0;
+    displayQuestion();
+  
+    //set total time depending on number of questions
+    time = questions.length * 10;
+  
+    //starts the function "countdown" every 1000ms to update time and displays it on the page
+    intervalID = setInterval(countdown, 1000);
+  
+    //put displayTime here to make sure time appears on the page as soon as the start button is clicked, not after 1 second.
+    displayTime();
+  }
+  
+  //reduce time by 1 and display new value, if time runs out then end quiz
+  function countdown() {
+    time--;
+    displayTime();
+    if (time < 1) {
+      endQuiz();
+    }
+  }
+  
+  //display time on page
+  const timeDisplay = document.querySelector("#time");
+  function displayTime() {
+    timeDisplay.textContent = time;
+  }
+  
+  //display the question and answer options for the current question
+  function displayQuestion() {
+    let question = questions[currentQuestion];
+    let options = question.options;
+  
+    let h2QuestionElement = document.querySelector("#question-text");
+    h2QuestionElement.textContent = question.questionText;
+  
+    for (let i = 0; i < options.length; i++) {
+      let option = options[i];
+      let optionButton = document.querySelector("#option" + i);
+      optionButton.textContent = option;
+    }
+  }
+  
+  //behaviour when an answer button is clicked: click event bubbles up to div with id "quiz-options"
+  //eventObject.target identifies the specific button element that was clicked on
+  document.querySelector("#quiz-options").addEventListener("click", checkAnswer);
+  
+  //Compare the content of the option button with the answer to the question
+  function optionIsCorrect(optionButton) {
+    return optionButton.textContent === questions[currentQuestion].answer;
+  }
+  
+  //if answer is wrong, reduce time
+  function checkAnswer(eventObject) {
+    let optionButton = eventObject.target;
+    resultDiv.style.display = "block";
+    if (optionIsCorrect(optionButton)) {
+      resultText.textContent = "Correct!";
+      setTimeout(hideResultText, 1000);
+    } else {
+      resultText.textContent = "Incorrect!";
+      setTimeout(hideResultText, 1000);
+      if (time >= 10) {
+        time = time - 10;
+        displayTime();
+      } else {
+        //if time is less than 10, display time as 0 and end quiz
+        //time is set to 0 in this case to avoid displaying a negative number if user picks a wrong answer with less 10 in the time remaining
+        time = 0;
+        displayTime();
+        endQuiz();
+      }
+    }
+  
+    //increase question by 1
+    currentQuestion++;
+    //keep showing remaining question or else end the quiz
+    if (currentQuestion < questions.length) {
+      displayQuestion();
+    } else {
+      endQuiz();
+    }
+  }
+  
+  //display the scorecard and hide other div's
+  const score = document.querySelector("#score");
+  
+  //at end of quiz, time is cleared, visible cards are hidden and score card is used to show the reamining time.
+  function endQuiz() {
+    clearInterval(intervalID);
+    hideCards();
+    scoreCard.removeAttribute("hidden");
+    score.textContent = time;
+  }
+  
+  const submitButton = document.querySelector("#submit-button");
+  const inputElement = document.querySelector("#initials");
+  
+  //store user initials and score when submit button is clicked
+  submitButton.addEventListener("click", storeScore);
+  
+  function storeScore(event) {
+    
+    event.preventDefault();
+  
+    //checks to see if user typed something in
+    if (!inputElement.value) {
+      alert("Please enter your initials before pressing submit!");
+      return;
+    }
+  
+    //score and initials kept in object
+    let leaderboardItem = {
+      initials: inputElement.value,
+      score: time,
+    };
+  
+    updateStoredLeaderboard(leaderboardItem);
+  
+    //hides the question card, and displays the leaderboardcard
+    hideCards();
+    leaderboardCard.removeAttribute("hidden");
+  
+    renderLeaderboard();
+  }
+  
+  //updates the leaderboard 
+  function updateStoredLeaderboard(leaderboardItem) {
+    let leaderboardArray = getLeaderboard();
+    // attach new leaderboard item to leaderboard array
+    leaderboardArray.push(leaderboardItem);
+    localStorage.setItem("leaderboardArray", JSON.stringify(leaderboardArray));
+  }
+  
+  //get "leaderboardArray" from local storage (if it exists) and parse it into a javascript object using JSON.parse
+  function getLeaderboard() {
+    let storedLeaderboard = localStorage.getItem("leaderboardArray");
+    if (storedLeaderboard !== null) {
+      let leaderboardArray = JSON.parse(storedLeaderboard);
+      return leaderboardArray;
+    } else {
+      leaderboardArray = [];
+    }
+    return leaderboardArray;
+  }
+  
+  //display leaderboard on leaderboard card
+  function renderLeaderboard() {
+    let sortedLeaderboardArray = sortLeaderboard();
+    const highscoreList = document.querySelector("#highscore-list");
+    highscoreList.innerHTML = "";
+    for (let i = 0; i < sortedLeaderboardArray.length; i++) {
+      let leaderboardEntry = sortedLeaderboardArray[i];
+      let newListItem = document.createElement("li");
+      newListItem.textContent =
+        leaderboardEntry.initials + " - " + leaderboardEntry.score;
+      highscoreList.append(newListItem);
+    }
+  }
+  
+  // leaderboard array is shown from highest to lowest
+  function sortLeaderboard() {
+    let leaderboardArray = getLeaderboard();
+    if (!leaderboardArray) {
+      return;
+    }
+  
+    leaderboardArray.sort(function (a, b) {
+      return b.score - a.score;
+    });
+    return leaderboardArray;
+  }
+  
+  const clearButton = document.querySelector("#clear-button");
+  clearButton.addEventListener("click", clearHighscores);
+  
+  //clears the local storage and displays an empty leaderboard
+  function clearHighscores() {
+    localStorage.clear();
+    renderLeaderboard();
+  }
+  
+  const backButton = document.querySelector("#back-button");
+  backButton.addEventListener("click", returnToStart);
+  
+  //Hides leaderboard card to show start card
+  function returnToStart() {
+    hideCards();
+    startCard.removeAttribute("hidden");
+  }
+  
+  //can view highscores at anytime
+  const leaderboardLink = document.querySelector("#leaderboard-link");
+  leaderboardLink.addEventListener("click", showLeaderboard);
+  
+  function showLeaderboard() {
+    hideCards();
+    leaderboardCard.removeAttribute("hidden");
+  
+    //stop countdown
+    clearInterval(intervalID);
+  
+    //time does not appear on page
+    time = undefined;
+    displayTime();
+  
+    //display leaderboard on leaderboard card
+    renderLeaderboard();
+  }
